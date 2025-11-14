5c7fbd6e9ccb68f98e11fabb97f3d6be4d54fb1b
Arthur Teixeira Silva <arthur.teixeira.git11@gmail.com>
2025-11-14 16:48:37 -0300

    commit 14/11/25
    refactor: adição de uma validação de dados na entrada de nome de usuário do sistema da agenda e nas funções de editar, remover e adicionar na agenda de contatos;
    
    feat: adição de uma opção de cancelar no pedido de dados para adicionar um contato na agenda (ainda não finalizada).

--- agenda.js
+++ agenda.js
@@ -16,44 +16,63 @@ function funcaoVisualizar(matrizContatos){
     return mostrar;
 };
 
-// 3.FUNÇÃO PARA REMOVER UM CONTATO(FEITO);
+// 3.FUNÇÃO PARA REMOVER UM CONTATO(FEITO)(VALIDAÇÃO CONCLUIDA!);
 function funcaoRemover(){
     const itemRemover = prompt("Digite o nome ou o telefone do contato que deseja remover: ")
+    while(itemRemover === null || itemRemover === "" || itemRemover <= 0){
+        itemRemover = prompt("Digite um nome ou número de contato valido: ")
+    }
     for(let i = 1; i < matrizContatos.length; i++){
-        if(itemRemover === matrizContatos[i][0] || itemRemover === matrizContatos[i][1]){
+        if(itemRemover === matrizContatos[i][0] || itemRemover === matrizContatBos[i][1]){
             alert("Contato encontrado!")
             matrizContatos.splice(i, 1)
-            alert(`Contato de nome ${i.valueOf} de número ${i.valueOf} foi removido`)
-            alert(`Contato removido`)
+            alert(`Contato de nome ${matrizContatos[i][0]} de número ${matrizContatos[i][1]} foi removido`)
             break;
         }
+        else{
+            alert(`o nome/número ${itemRemover} não existe na sua agenda de contatos!`)
+            break;
+        };
     }
 }
 
 
-// 4.FUNÇÃO PARA EDITAR UM CONTATO(AINDA EM DESENVOLVIMENTO.);
+// 4.FUNÇÃO PARA EDITAR UM CONTATO(VALIDAÇÃO CONCLUIDA!);
 function funcaoEditar(){
-    const itemEditar = prompt("Digite o nome ou o telefone do cotato que deseja editar: ")
+    let itemEditar = prompt("Digite o nome ou o telefone do contato que deseja editar: ")
+    while(itemEditar === null || itemEditar === "" || itemEditar <= 0){
+        itemEditar = prompt("Digite um nome ou número de contato valido: ");
+    };
+    if(itemEditar !== matrizContatos.values){
+        alert(`o nome/número ${itemEditar} não existe na sua agenda de contatos`)
+        alert("Voltando para o menu inicial...")
+    }
     for(let i = 1; i < matrizContatos.length; i++){
         if(itemEditar == matrizContatos[i][0] || itemEditar == matrizContatos[i][1]){
             alert("Contato encontrado!");
             let flagEdit = true;
             let opcaoEditar = "";
+        
             do{
-                opcaoEditar = parseInt(prompt("Digite 1 para editar o nome do contato ou 2 para editar o número de telefone"));
+                opcaoEditar = parseInt(prompt("Digite 1 para editar o nome do contato ou 2 para editar o número de telefone ou 3 para sair do modo de edição"));
 
                 switch(opcaoEditar){
                     case 1:
                         let newNomContato = prompt("Digite o novo nome do contato: ");
+                        while(newNomContato === null || newNomContato === "" || !isNaN(newNomContato)){
+                            newNomContato = prompt("Digite um nome de contato valido: ")
+                        }
                         matrizContatos[i][0] = newNomContato;
                         break;
                     case 2:
                         let newNumContato = prompt("Digite o novo número do contato: ");
+                        while(newNumContato === null || newNumContato === "" || newNumContato <= 0 || isNaN(newNumContato)){
+                            newNumContato = prompt("Digite um número de contato valido: ")
+                        };
                         matrizContatos[i][1] = newNumContato
                         break;
                     case 3:
                         alert("Operação cancelada!")
-                        alert("saindo...")
                         flagEdit = false
                     default:
                         alert("Digite algo valido!")
@@ -64,23 +83,32 @@ function funcaoEditar(){
     };
 };
 
-//5.FUNÇÃO PARA PEDIR DADOS(FEITO);
+//5.FUNÇÃO PARA PEDIR DADOS(FEITO)(VALIDAÇÃO CONCLUIDA!)(EM DESENVOLVIMENTO);
 function funcaoDados(){
-    let nomContato = prompt("Digite o nome do contato:");
+    let nomContato = prompt("Digite o nome do contato ou digite 'cancelar' para sair :");
+    while(nomContato === null || nomContato === "" || !isNaN(nomContato)){
+        nomContato = prompt("Por favor digite o nome do contato corretamente: ")
+    };
+    if(nomContato === cancelar){
+        alert("Voltando para o menu incial...")
+    }
     let numContato = prompt("Digite o número do contato: ");
+    while(numContato === null || numContato <= 0 || numContato === "" || isNaN(numContato)){
+        numContato = prompt("Digite o número do contato corretamente: ")
+    };
     const arrayContato = new Array(nomContato, numContato);
     return arrayContato;
 };
 
-// CRIAÇÃO DA MATRIZ QUE IRÁ ARAMZENAR OS CONTATOS;
+// CRIAÇÃO DA MATRIZ QUE IRÁ ARMAZENAR OS CONTATOS;
 const matrizContatos = [
-    ["Nome", "Telefone"]
+    ["Nome:", "Telefone:"]
 ];
 
 //MENU PARA NAVEGAÇÃO DA AGENDA (SWITCH CASE + DO/WHILE)(FEITO);
 let flagMenu = true;
 let opcao = ""
-//(VALIDAÇÃO DO NOME DO USUÁRIO FEITA)
+//(VALIDAÇÃO CONCLUIDA!)
 let usu = prompt("Digite seu nome do usuário: ")
 while(usu === null || usu === "" || !isNaN(usu)){
     usu = prompt("Resultado invalido, digite o seu nome corretamente: ")
